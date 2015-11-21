7cdd0fb9205be943300bf33e92ceb4b93138f4aa
Marin Bek <marin.bek@gmail.com>
2015-11-21 16:17:06 +0100

    Better header test

--- netheader_test.go
+++ netheader_test.go
@@ -12,24 +12,21 @@
 package pubsubsql
 
 import (
-	"fmt"
-	"testing"
+	"encoding/binary"
+	. "gopkg.in/check.v1"
 )
 
-func TestNetworkHeader(t *testing.T) {
-	header1 := netHeader{
-		MessageSize: 32567,
-		RequestId:   9875235,
-	}
-	var header2 netHeader
-	bytes := make([]byte, 100, 100)
-	//
-	header1.writeTo(bytes)
-	header2.readFrom(bytes)
-	//
-	if header1 != header2 {
-		t.Error("NetworkHeader data does not match")
-	}
+func (s *TestSuite) TestreadFrom(c *C) {
+	nh := netHeader{}
 
-	fmt.Println(header1.String())
+	source_bytes := []byte("123456789")
+	nh.readFrom(source_bytes)
+
+	destination := make([]byte, 15, 15)
+	nh.writeTo(destination)
+	// First 8 bytes of the header need to match
+	c.Assert(source_bytes[:8], DeepEquals, destination[:8])
+
+	c.Assert(nh.RequestId, Equals, binary.BigEndian.Uint32([]byte("5678")))
+	c.Assert(nh.MessageSize, Equals, binary.BigEndian.Uint32([]byte("1234")))
 }
